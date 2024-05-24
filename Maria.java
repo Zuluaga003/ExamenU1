35b75234cdf716f58051c962f741f0ce5eda66fc
Zuluaga003 <20233tn033@utez.edu.mx>
2024-05-24 11:01:58 -0600

    Java class de maria

---
+++ Maria.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Maria {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo! Maria");
+
+    }
+}
---
+++ pedro.java
@@ -0,0 +1,8 @@
+package org.example;
+
+
+public class pedro {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo! Pedro");
+    }
+}
